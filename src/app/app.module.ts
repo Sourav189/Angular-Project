443440467a89ea695017a21ec918a4552738b7e3
Sourav Singh <souravmahi18@gmail.com>
2020-05-03 04:08:46 +0530

    2nd component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -22,7 +26,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],//provide all the services
   bootstrap: [AppComponent]
