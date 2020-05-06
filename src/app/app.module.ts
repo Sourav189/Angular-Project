417b4f5111a1134b5958e377691ab359e228f01f
Sourav Singh <souravmahi18@gmail.com>
2020-05-07 03:43:10 +0530

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,16 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailsComponent } from './dishdetails/dishdetails.component';
 import { DishService } from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailsComponent
+    DishdetailsComponent,
+    HeaderComponent,
+    FooterComponent
     //specifies the view classes that belong to a particular module
     //view classes are in the form of containers directives and pipes
   ],
