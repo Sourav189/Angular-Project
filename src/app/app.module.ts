faa574f7e708b32d7a21f958de58cd013fb79e48
Sourav Singh <souravmahi18@gmail.com>
2020-05-06 23:29:23 +0530

    Basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailsComponent } from './dishdetails/dishdetails.component';
+import { DishService } from "./services/dish.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailsComponent
     //specifies the view classes that belong to a particular module
     //view classes are in the form of containers directives and pipes
   ],
@@ -31,7 +34,9 @@ import { MenuComponent } from './menu/menu.component';
     MatGridListModule,
     MatButtonModule
   ],
-  providers: [],//provide all the services
+  providers: [
+    DishService
+  ],//provide all the services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
