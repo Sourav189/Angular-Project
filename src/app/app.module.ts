6c74189829a9d0e5f3fe5f1d73744444b2f1edb4
Sourav Singh <souravmahi18@gmail.com>
2020-05-07 18:49:23 +0530

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ import { DishdetailsComponent } from './dishdetails/dishdetails.component';
 import { DishService } from "./services/dish.service";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
     //specifies the view classes that belong to a particular module
     //view classes are in the form of containers directives and pipes
   ],
@@ -36,7 +44,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
