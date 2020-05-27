4a5c26e8b9035459d5deaed89ec939a3da36989a
Sourav Singh <souravmahi18@gmail.com>
2020-05-27 18:37:09 +0530

    Angular promises part1

--- src/app/dishdetails/dishdetails.component.ts
+++ src/app/dishdetails/dishdetails.component.ts
@@ -26,7 +26,9 @@ export class DishdetailsComponent implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
-    this.dish = this.dishservice.getDish(id);
+     this.dishservice.getDish(id)
+       .then(dish => this.dish = dish);
+
   }
 
   goBack(): void {
